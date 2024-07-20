e34934b1707b728039739168087fedb8e7df632f
fareedjaved203 <fareedjaved203@gmail.com>
2024-07-20 17:05:33 +0500

    hover animation added

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 
 const Footer = () => {
@@ -9,26 +8,57 @@ const Footer = () => {
         style={{ fontSize: "13px", fontWeight: "500" }}
       >
         <div class="flex flex-nowrap flex-col md:w-2/3 justify-center md:flex-row items-center space-y-6 md:space-y-0">
-          <div className="text-[#FFDC23] text-2xl">Musa.</div>
+          <Link href={"/"}>
+            <div
+              className="text-[#FFDC23] cursor-pointer"
+              style={{ fontSize: "16px", fontWeight: "700" }}
+            >
+              Musa.
+            </div>
+          </Link>
 
           <nav
             class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center hover:cursor-pointer"
-            style={{ fontSize: "13px", fontWeight: "500" }}
+            style={{ fontSize: "16px", fontWeight: "400" }}
           >
-            <a class="mr-5 hover:underline ">Home</a>
-            <a class="mr-5 hover:underline">My Work</a>
-            <a class="mr-5 hover:underline">Featured</a>
-            <a class="mr-5 hover:underline flex justify-center items-center space-x-1">
+            <Link href="/" class="mr-5 relative inline-block group ">
+              Home
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-white origin-left transform scale-x-0 group-hover:scale-x-100 transition-transform duration-450 ease-in-out"></span>
+            </Link>
+
+            <Link href="/work" class="mr-5 relative inline-block group">
+              My Work
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-white origin-left transform scale-x-0 group-hover:scale-x-100 transition-transform duration-450 ease-in-out"></span>
+            </Link>
+
+            <Link href="/#feature" class="mr-5 relative inline-block group">
+              Featured
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-white origin-left transform scale-x-0 group-hover:scale-x-100 transition-transform duration-450 ease-in-out"></span>
+            </Link>
+            <Link href="/#about" class="mr-5 relative inline-block group">
               About Me
-            </a>
-            <a class="mr-5 hover:underline">Testimonials</a>
-            <a class="mr-5 hover:underline">Results</a>
-            <a class="mr-5 hover:underline">FAQs</a>
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-white origin-left transform scale-x-0 group-hover:scale-x-100 transition-transform duration-450 ease-in-out"></span>
+            </Link>
+            <Link
+              href="/#testimonials"
+              class="mr-5 relative inline-block group"
+            >
+              Testimonials
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-white origin-left transform scale-x-0 group-hover:scale-x-100 transition-transform duration-450 ease-in-out"></span>
+            </Link>
+            <Link href="/#results" class="mr-5 relative inline-block group">
+              Results
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-white origin-left transform scale-x-0 group-hover:scale-x-100 transition-transform duration-450 ease-in-out"></span>
+            </Link>
+            <Link href="/#faqs" class="mr-5 relative inline-block group">
+              FAQs
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-white origin-left transform scale-x-0 group-hover:scale-x-100 transition-transform duration-450 ease-in-out"></span>
+            </Link>
           </nav>
           <Link href={"/work"}>
             <button
-              class="border-2 border-[#FFDC23] bg-[#FFDC23] text-[#FFDC23] text-black text-center rounded-lg px-8 py-3 text-nowrap font-bold"
-              style={{ fontSize: "14px" }}
+              class="border-2 border-[#FFDC23] bg-[#FFDC23] text-black text-center rounded-lg px-8 py-3 text-nowrap"
+              style={{ fontSize: "16px", fontWeight: "700" }}
             >
               Contact us
             </button>
--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -11,40 +11,56 @@ const Navbar = () => {
         style={{ fontSize: "13px", fontWeight: "500" }}
       >
         <div class="container mx-auto px-4 py-5 w-2/3 justify-center flex flex-nowrap flex-col md:flex-row items-center">
-          <div className="text-[#FFDC23] text-2xl">Musa.</div>
+          <Link href={"/"}>
+            <div
+              className="text-[#FFDC23] cursor-pointer"
+              style={{ fontSize: "16px", fontWeight: "700" }}
+            >
+              Musa.
+            </div>
+          </Link>
           <nav
             class="font-roboto md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center hover:cursor-pointer "
-            style={{ fontSize: "13px", fontWeight: "500" }}
+            style={{ fontSize: "16px", fontWeight: "400" }}
           >
-            <Link href="/" class="mr-5 hover:underline ">
+            <Link href="/" class="mr-5 relative inline-block group ">
               Home
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-white origin-left transform scale-x-0 group-hover:scale-x-100 transition-transform duration-450 ease-in-out"></span>
             </Link>
-            <Link href="/work" class="mr-5 hover:underline">
+
+            <Link href="/work" class="mr-5 relative inline-block group">
               My Work
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-white origin-left transform scale-x-0 group-hover:scale-x-100 transition-transform duration-450 ease-in-out"></span>
             </Link>
-            <Link href="/#feature" class="mr-5 hover:underline">
+
+            <Link href="/#feature" class="mr-5 relative inline-block group">
               Featured
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-white origin-left transform scale-x-0 group-hover:scale-x-100 transition-transform duration-450 ease-in-out"></span>
+            </Link>
+            <Link href="/#about" class="mr-5 relative inline-block group">
+              About Me
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-white origin-left transform scale-x-0 group-hover:scale-x-100 transition-transform duration-450 ease-in-out"></span>
             </Link>
             <Link
-              href="/#about"
-              class="mr-5 hover:underline flex justify-center items-center space-x-1"
+              href="/#testimonials"
+              class="mr-5 relative inline-block group"
             >
-              About Me
-            </Link>
-            <Link href="/#testimonials" class="mr-5 hover:underline">
               Testimonials
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-white origin-left transform scale-x-0 group-hover:scale-x-100 transition-transform duration-450 ease-in-out"></span>
             </Link>
-            <Link href="/#results" class="mr-5 hover:underline">
+            <Link href="/#results" class="mr-5 relative inline-block group">
               Results
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-white origin-left transform scale-x-0 group-hover:scale-x-100 transition-transform duration-450 ease-in-out"></span>
             </Link>
-            <Link href="/#faqs" class="mr-5 hover:underline">
+            <Link href="/#faqs" class="mr-5 relative inline-block group">
               FAQs
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-white origin-left transform scale-x-0 group-hover:scale-x-100 transition-transform duration-450 ease-in-out"></span>
             </Link>
           </nav>
           <Link href={"/work"}>
             <button
-              class="border-2 border-[#FFDC23] bg-[#FFDC23] text-[#FFDC23] text-black text-center rounded-lg px-8 py-3 text-nowrap font-bold"
-              style={{ fontSize: "14px" }}
+              class="border-2 border-[#FFDC23] bg-[#FFDC23] text-black text-center rounded-lg px-8 py-3 text-nowrap"
+              style={{ fontSize: "16px", fontWeight: "700" }}
             >
               Contact us
             </button>
