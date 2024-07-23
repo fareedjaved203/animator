3031e95c1f5b88490ffe6f6fbf3125734faeeed8
fareedjaved203 <fareedjaved203@gmail.com>
2024-07-23 23:44:49 +0500

    ui improved

--- components/work/Grid.jsx
+++ components/work/Grid.jsx
@@ -4,19 +4,19 @@ import work from "./work.json";
 const GridComponent = () => {
   return (
     <div className="flex justify-center items-center">
-      <div className="grid grid-cols-2 md:grid-cols-4 md:w-[75%] xl:2/3">
+      <div className="grid grid-cols-2 md:grid-cols-4 md:w-[1192px] place-items-center">
         {work.map((item) => (
           <div
             key={item.id}
-            className="rounded-xl m-2 overflow-hidden cursor-pointer hover:scale-105 hover:transition-all hover:duration-300 duration-300"
+            className="rounded-[12.73px] my-2 overflow-hidden cursor-pointer hover:scale-105 hover:transition-all hover:duration-300 duration-300"
           >
-            <div className="w-full rounded-[20px] overflow-hidden shadow-lg flex justify-center items-center">
+            <div className="flex justify-center items-center">
               <Image
                 src={item.image}
                 width={360}
                 height={360}
                 alt="image"
-                className="cover rounded-[20px] border-3 border-blue-400"
+                className="cover w-[268px] h-[151px] rounded-[12.73px]"
               />
             </div>
           </div>
